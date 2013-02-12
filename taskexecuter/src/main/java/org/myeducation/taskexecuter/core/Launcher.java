0b61ca7c68b1fc9251a6dc5af12485716af2ffa5
Gribov Kirill <kgribov@griddynamics.com>
2013-02-13 01:06:30 +0400

    Add task executer module

---
+++ taskexecuter/src/main/java/org/myeducation/taskexecuter/core/Launcher.java
@@ -0,0 +1,14 @@
+package org.myeducation.taskexecuter.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kirilkadurilka
+ * Date: 13.02.13
+ * Time: 1:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class Launcher {
+    public static void main(String[] args){
+        //here will run task executer process
+    }
+}
